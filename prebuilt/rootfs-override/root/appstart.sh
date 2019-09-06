f308f4ab5e284817d80aa85c8cf3dff020163702
liud <liud>
2019-09-06 08:49:46 +0800

    open gc0308

--- prebuilt/rootfs-override/root/appstart.sh
+++ prebuilt/rootfs-override/root/appstart.sh
@@ -2,6 +2,9 @@
 
 # add your app start script here
 
+echo "nameserver 114.114.114.114" > /etc/resolv.conf
+mkdir -p /root/sdcard/
+mount /dev/mmcblk0p2 /root/sdcard/
 
 echo "****************************************************"
 echo "*                                                  *"
